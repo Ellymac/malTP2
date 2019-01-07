3715b230c6e6d442d1cb87bd60d52a41b0aacfb9
Ellymac <cschnell67@gmail.com>
2019-01-07 18:34:32 +0100

    final file : caesarian.R

---
+++ projet/caesarian.R
@@ -0,0 +1,97 @@
+# Hirtz Hubert - Schnell Camille
+
+library(class);
+library(e1071);
+library(foreign);
+library(ggplot2);
+library(purrr);
+library(randomForest);
+library(rsample);
+library(tibble);
+
+rm(list=ls());
+graphics.off();
+
+n = 80;
+
+#### Read and store data ####
+caesarians = readARFF("caesarian.csv.arff")
+names(caesarians) = c("Age", "DN", "DT", "BP", "HP", "Caesarian");
+
+# change Age and Delivery number columns to numeric values
+caesarians$Age = as.numeric(as.character(caesarians$Age))
+caesarians$DN = as.numeric(as.character(caesarians$DN))
+str(caesarians)
+
+# separate data into 2 : one set to train the model, one used to test it
+indexes = sample(80, 60)
+data_train = caesarians[indexes,]
+data_test = caesarians[-indexes,]
+
+
+#### CART tree ####
+caesarianTree <- rpart(Caesarian~., data = data_train,control=rpart.control(minsplit=10, cp=0))
+
+# optimize tree
+#plotcp(caesarianTree)
+#caesarianTreeOpti <- prune(caesarianTree,cp=caesarianTree$cptable[which.min(caesarianTree$cptable[,4]),1])
+prp(caesarianTree, extra = 1)
+
+predTree <- predict(caesarianTree, newdata = data_test, type = "class")
+# confusion matrix and performance
+table(data_test$Caesarian, predTree)
+Tree_perf <- mean(predTree == data_test$Caesarian)
+
+
+#### Random Forest ####
+rf = randomForest(Caesarian~., data = data_train)
+rfpred_test <- predict(rf, newdata = data_test, type = "class")
+Rf_test <- mean(rfpred_test == data_test$Caesarian)
+
+# check confusion matrix
+print(rf)
+# most important variables --> age, and then blood pressure
+varImpPlot(rf)
+
+
+#### Fold to compare models ####
+folds = vfold_cv(caesarians, v=5);
+performance = map_dfr(folds$splits, function (x) {
+  x.train = as_tibble(x, data = "analysis");
+  x.test = as_tibble(x, data = "assessment");
+  
+  perf_of_bayes = function (m) {
+    y_pred = predict(m, newdata = x.test);
+    mean(y_pred == x.test$Caesarian)
+  };
+  
+  perf_of_knn = function (k) {
+    model = knn(x.train, x.test, cl = x.train$Caesarian, k = k);
+    mean(x.test$Caesarian == model)
+  };
+  
+  perf_of = function (m) {
+    y_pred = predict(m, newdata = x.test, type = "class");
+    mean(y_pred == x.test$Caesarian)
+  };
+  
+  bayes = perf_of_bayes(naiveBayes(x.train$Caesarian~., data = x.train));
+  tree_ = perf_of(tree(x.train$Caesarian~., data = x.train));
+  cart = perf_of(rpart(x.train$Caesarian~., data = x.train,control=rpart.control(minsplit=10, cp=0)));
+  randomForest = perf_of(randomForest(x.train$Caesarian~., data = x.train));
+  bagging = perf_of(bagging(x.train$Caesarian~., data = x.train));
+  knn01 = perf_of_knn(1);
+  knn05 = perf_of_knn(5);
+  knn20 = perf_of_knn(20);
+  knn = max(max(knn01, knn05), knn20);
+  
+  tibble( bayes = bayes
+          , tree = tree_
+          , cart = cart
+          , randomForest = randomForest
+          , bagging = bagging
+          , knn = knn)
+});
+
+performance = gather(performance, key="Algorithm", value="Error");
+ggplot(data = performance,aes(x=Algorithm, y=Error,col=Algorithm)) + geom_boxplot();
--- projet/caesarian_cart.R
+++ projet/caesarian_cart.R
@@ -4,6 +4,8 @@ library(ipred)
 library(tree)
 library(randomForest)
 library(class)
+library(rpart)
+library(rpart.plot)
 
 rm(list=ls());
 graphics.off();
@@ -12,44 +14,65 @@ graphics.off();
 dataset = readARFF("caesarian.csv.arff")
 
 str(dataset)
+dataset$Age = as.numeric(as.character(dataset$Age))
+dataset$Deliverynumber = as.numeric(as.character(dataset$Deliverynumber))
+
 
 # separate data into 2 : one set to train the model, one used to test it
 indexes = sample(80, 60)
 data_train = dataset[indexes,]
 data_test = dataset[-indexes,]
 
+caesarianTree <- rpart(Caesarian~., data = data_train,control=rpart.control(minsplit=15, cp=0))
+plotcp(caesarianTree)
+
+caesarianTreeOpti <- prune(caesarianTree,cp=caesarianTree$cptable[which.min(caesarianTree$cptable[,4]),1])
+prp(caesarianTreeOpti, extra = 1)
+
+predTree <- predict(caesarianTreeOpti, newdata = data_test, type = "class")
+table(data_test$Caesarian, predTree)
+Tree_perf <- mean(predTree == data_test$Caesarian)
 
 # knn predict for k = 1 , k = 5 and k = 20
 
-knn.1 = knn(data_train, data_test, cl = dataset$Caesarian[indexes], k = 1)
-perf_knn.1 = 100 * sum(dataset$Caesarian[-indexes] == knn.1)/100
+#knn.1 = knn(data_train, data_test, cl = dataset$Caesarian[indexes], k = 1)
+#perf_knn.1 = 100 * sum(dataset$Caesarian[-indexes] == knn.1)/100
 
-knn.5 = knn(data_train, data_test, cl = dataset$Caesarian[indexes], k = 5)
-perf_knn.5 = 100 * sum(dataset$Caesarian[-indexes] == knn.5)/100
+#knn.5 = knn(data_train, data_test, cl = dataset$Caesarian[indexes], k = 5)
+#perf_knn.5 = 100 * sum(dataset$Caesarian[-indexes] == knn.5)/100
 
-knn.20 = knn(data_train, data_test, cl = dataset$Caesarian[indexes], k = 20)
-perf_knn.20 = 100 * sum(dataset$Caesarian[-indexes] == knn.20)/100
+#knn.20 = knn(data_train, data_test, cl = dataset$Caesarian[indexes], k = 20)
+#perf_knn.20 = 100 * sum(dataset$Caesarian[-indexes] == knn.20)/100
   
 
 # Bagging, LDA, tree, randomforest
 
-modbag = bagging(Caesarian~., data = data_train)
-bagpred_test <- predict(modbag, newdata = data_test, type = "class")
-Bagging_test <- mean(bagpred_test == data_test$Caesarian)
+#modbag = bagging(Caesarian~., data = data_train, coob = TRUE, nbagg = 10)
+#bagpred_test <- predict(modbag, newdata = data_test, type = "class")
+#Bagging_test <- mean(bagpred_test == data_test$Caesarian)
 #bagpred_train <- predict(modbag, newdata = data_train, type = "class")
 #Bagging_train <- mean(bagpred_train == data_train$Caesarian)
 
-lda_data = lda(Caesarian~., data = data_train)
-ldapred_test <- predict(lda_data, newdata = data_test, type = "class")
-LDA_test <- mean(ldapred_test == data_test$Caesarian)
+# check confusion matrix
+#print(modbag)
 
-tree_data = tree(Caesarian~., data = data_train)
-treepred_test <- predict(tree_data, newdata = data_test, type = "class")
-Tree_test <- mean(treepred_test == data_test$Caesarian)
+#lr = glm(data_train$Caesarian~., data = data_train, family = binomial(link = 'logit'))
+#summary(lr)
+#plot(lr)
+#lrpred_test <- predict(lr, newdata = data_test, type="response")
+#LR_test <- mean(lrpred_test == data_test$Caesarian)
+
+#lda_data = lda(data_train$Caesarian~., data = data_train)
+#ldapred_test <- predict(lda_data, newdata = data_test, type = "class")
+#LDA_test <- mean(ldapred_test == data_test$Caesarian)
 
 #rf = randomForest(Caesarian~., data = data_train)
 #rfpred_test <- predict(rf, newdata = data_test, type = "class")
 #Rf_test <- mean(rfpred_test == data_test$Caesarian)
+#rfpred_train <- predict(rf, newdata = data_train, type = "class")
+#Rf_train <- mean(rfpred_train == data_train$Caesarian)
 
-
-# naive bayes
+# check confusion matrix
+#print(rf)
+# most important variables --> age
+#varImpPlot(rf)
